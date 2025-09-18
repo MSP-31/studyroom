ff5c59fc3ddbe18af1732d1719aef653a7ded4cf
DESKTOP-CEKNQTC <jhss9747@gmail.com>
2025-09-18 19:14:21 +0900

    init: "기본설정"

---
+++ src/main/java/com/synclife/studyroom/conference/controller/RoomController.java
@@ -0,0 +1,10 @@
+package com.synclife.studyroom.conference.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class RoomController {
+
+}
